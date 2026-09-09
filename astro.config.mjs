28065623bdd25b6a8fcd12f360a3e215c658b250
James Quick <jquick@cloudflare.com>
2026-09-09 08:58:59 -0500

    feat: configure Weekender design foundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-export default defineConfig({});
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  output: "static",
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
